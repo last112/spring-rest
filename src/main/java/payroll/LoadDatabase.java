7d552e37b0ea803c770c7cb79bb1511acf79ee2d
last112 <lastfly4@gmail.com>
2019-10-20 10:02:28 +0530

    Spring-rest

---
+++ src/main/java/payroll/LoadDatabase.java
@@ -0,0 +1,24 @@
+package payroll;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(OrderRepository orderRepository) {
+        return args -> {
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> {
+                log.info("Preloaded " + order);
+            });
+        };
+    }
+}
